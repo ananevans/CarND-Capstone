5691d333fe24b532e5cc03876b10ade41da2d52e
Mohamed Elbanhawi <mohamedbanhawi@gmail.com>
2019-06-16 12:46:52 +0000

    somewhat stable parameters for speed/steering controller

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -11,15 +11,15 @@ class Controller(object):
     def __init__(self, vehicle_mass, fuel_capacity, brake_deadband, decel_limit, accel_limit, wheel_radius, wheel_base, steer_ratio, max_lat_accel, max_steer_angle):
         # TODO: Implement
         self.yaw_controller = YawController(wheel_base, steer_ratio, 0.1, max_lat_accel, max_steer_angle)
-        kp = 0.3
-        ki = 0.1
+        kp = 0.5
+        ki = 1e-5
         kd = 0.0
         mn = 0.0 # minimum throttle value
         mx = 0.2 # maximum throttle value
         self.throttle_controller = PID(kp, ki, kd, mn, mx)
         
-        tau = 0.5 # 1/(2pi*tau) = cutoff frequency
-        ts = 0.02 # sample time
+        tau = 0.6 # 1/(2pi*tau) = cutoff frequency
+        ts = 0.2 # sample time
         self.vel_lpf = LowPassFilter(tau, ts)
         
         self.vehicle_mass = vehicle_mass
